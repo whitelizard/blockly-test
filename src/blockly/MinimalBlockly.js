8dba9a81899d117d730b8cf144bad423e93fe81d
whitelizard <esbblo@gmail.com>
2018-03-25 15:15:53 +0200

    WIP

--- src/blockly/MinimalBlockly.js
+++ src/blockly/MinimalBlockly.js
@@ -24,6 +24,31 @@ const options = {
 
 class MinimalBlockly extends Component {
   componentDidMount() {
+    var blocklyArea = document.getElementById('blocklyArea');
+    var blocklyDiv = document.getElementById('blocklyDiv');
+    var workspacePlayground = Blockly.inject(blocklyDiv, {
+      toolbox: document.getElementById('toolbox')
+    });
+    var onresize = function(e) {
+      // Compute the absolute coordinates and dimensions of blocklyArea.
+      var element = blocklyArea;
+      var x = 0;
+      var y = 0;
+      do {
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
+      } while (element);
+      // Position blocklyDiv over blocklyArea.
+      blocklyDiv.style.left = x + 'px';
+      blocklyDiv.style.top = y + 'px';
+      blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+      blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    };
+    window.addEventListener('resize', onresize, false);
+    onresize();
+    Blockly.svgResize(workspacePlayground);
+
     this.workspace = Blockly.inject(this.area, options);
     const workspaceBlocks = document.getElementById('blockly-workspaceBlocks');
     Blockly.Xml.domToWorkspace(workspaceBlocks, this.workspace);
